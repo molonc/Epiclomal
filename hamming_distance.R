57eaa6ec370d7560875726b49c0b22c86b043ff7
Camila de Souza <cdesouza@bccrc.ca>
2017-07-20 13:50:26 -0700

    new version that allows starting counting from 0 or 1

--- hamming_distance.R
+++ hamming_distance.R
@@ -40,13 +40,13 @@ true_Z <- read.csv(true_Z_file,sep='\t')
 true_epi <- read.csv(true_epigenotypes_file,sep='\t',header=TRUE)
 
 true_epi.tmp <- as.matrix(true_epi[,-1])
-data_true <- (t(sapply(true_Z[,2],function(x){return(true_epi.tmp[x,])}))) ### obtaining the true epigenotype for each cell
+data_true <- (t(sapply(true_Z[,2],function(x){return(true_epi.tmp[which(true_epi[,1]==x),])}))) ### obtaining the true epigenotype for each cell
 
 estimate_Z <- read.csv(estimated_Z_file,sep='\t')
 estimate_epi <- read.csv(estimated_epigenotypes_file,sep='\t',header=TRUE)
 
 estimate_epi.tmp <- as.matrix(estimate_epi[,-1])
-data_estimate <- (t(sapply(estimate_Z[,2],function(x){return(estimate_epi.tmp[x,])}))) 
+data_estimate <- (t(sapply(estimate_Z[,2],function(x){return(estimate_epi.tmp[which(estimate_epi[,1]==x),])})))
 
 ncells <- dim(data_true)[1]
 nloci <- dim(data_true)[2]
